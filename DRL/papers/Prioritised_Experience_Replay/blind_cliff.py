83c07a88ec5e2f57682f8deb36652435e709a88c
Rowing0914 <kosakaboat@gmail.com>
2019-03-20 22:54:47 +0000

    Added

--- DRL/papers/Prioritised_Experience_Replay/blind_cliff.py
+++ DRL/papers/Prioritised_Experience_Replay/blind_cliff.py
@@ -5,12 +5,13 @@ Date: 20 Mar 2019
 Description:
   This is the rough implementation of the Q-learnig agent on the Blind Cliffwalk environment.
   In the paper, Figure 1 describes the uniformly random agent linearly increases the number of updates needed, whrereas Q-learnign agent does not.
-  So to confirm, if it is true, I have implemented the thing.
+  So to reproduce the result, I have implemented the thing.
 
 """
 
 import numpy as np
-from collections import defaultdict, deque
+from collections import deque
+from random import shuffle
 
 class blind_cliff:
   """
@@ -105,18 +106,22 @@ class blind_cliff:
     temp[self.agent_pos] += 1
     return temp
 
+
+
 class Agent:
   """
   Q-learning agent
   """
-  def __init__(self, Q, alpha, gamma, eta, epsilon, memory_size, state_space, action_n):
+  def __init__(self, alpha, gamma, eta, epsilon, memory_size, batch_size, state_space, action_n, is_prioritised, Q=None):
     self.epsilon = epsilon
-    self.Q       = Q
     self.alpha   = alpha
     self.gamma   = gamma
     self.memory  = deque(maxlen=memory_size) # EXPERIECNE REPLAY MEMORY
     self.weights = np.random.normal(0, 0.01, [state_space, action_n]) # gaussian distribution: mean 0 and sigma 0.01
     self.eta     = eta
+    self.batch_size = batch_size
+    self.is_prioritised = is_prioritised
+    self.Q       = Q
 
   def __repr__(self):
     """
@@ -154,24 +159,40 @@ class Agent:
     """
     return np.dot(state, self.weights) + 1.0 # 1 is bias
 
-  def update(self, state, action, reward, next_state, done):
+  def update(self):
     """
     Update weights used in function approximation by SGD
     tricky thing is that the error we want to propagate is not MSE or usual thinsg in ML literature
     here we use the TD error to learn.
     """
-    td_delta = reward + self.gamma*np.max(self.function_approximator(next_state)) - self.function_approximator(state)[action]
-    self.weights += self.eta*td_delta*self.function_approximator(state)[action]
 
-  def _memory_maintainance(self):
-    return
+    if self.is_prioritised:
+      states, actions, rewards, next_states, dones = self._prioritise_sample()
+    else:
+      states, actions, rewards, next_states, dones = self._sample()
+    
+    for state, action, reward, next_state, done in zip(states, actions, rewards, next_states, dones):
+      td_delta = reward + self.gamma*np.max(self.function_approximator(next_state)) - self.function_approximator(state)[action]
+      self.weights += self.eta*td_delta*self.function_approximator(state)[action]
 
-  def old_update(self, state, action, reward, next_state, done):
+  def _sample(self):
     """
-    Update the Q-table
+    randomly select a chunk of experiences to break the correlation in history(memory)
     """
-    td_delta = reward + self.gamma*np.max(self.Q[next_state]) - self.Q[state][action]
-    self.Q[state][action] += self.alpha*td_delta
+    indices = np.random.choice(len(self.memory), self.batch_size, replace=False)
+    states, actions, rewards, next_states, dones = zip(*[self.memory[idx] for idx in indices])
+    return states, actions, rewards, next_states, dones
+
+  def _prioritise_sample(self):
+    """
+    select a chunk of experiences which only contains good experiences
+    """
+
+    def getKey(item):
+      # sort the memory by rewards
+      return item[2]
+    states, actions, rewards, next_states, dones = zip(*sorted(list(self.memory), key=getKey, reverse=True)[:self.batch_size])
+    return states, actions, rewards, next_states, dones
 
 if __name__ == '__main__':
   EPSILON = 0.99
@@ -179,16 +200,19 @@ if __name__ == '__main__':
   ETA   = 1./4
   NUM_EPISODES = 100
   MEMORY_SIZE = 200
-  logs = list()
+  BATCH_SIZE = 16
+  FREQUENCY_UPDATE = 20
+  logs_PER, logs_NONPER = list(), list()
 
   for n in range(2, 16):
     env = blind_cliff(n) # GAME ENV
     GAMMA = 1 - 1./n
-    Q = defaultdict(lambda: np.random.normal(0, 0.01, env.action_n)) # Q TABLE
-    agent = Agent(Q, ALPHA, GAMMA, ETA, EPSILON, MEMORY_SIZE, env.n, env.action_n) # Q-LEARNING AGENT
+
+    # Q-learning with Prioritised Experience Replay
+    agent = Agent(ALPHA, GAMMA, ETA, EPSILON, MEMORY_SIZE, BATCH_SIZE, env.n, env.action_n, is_prioritised=True)
     rewards = 0
 
-    for i in range(NUM_EPISODES):
+    for i in range(1,NUM_EPISODES):
       state = env.reset()
       while True:
         # if you want to see how the agent moves, open this!
@@ -196,13 +220,45 @@ if __name__ == '__main__':
 
         action = agent.policy(state)
         next_state, reward, done = env.step(action)
-        agent.update(state, action, reward, next_state, done)
+        agent.memory.append((state, action, reward, next_state, done))
         if done:
           rewards += reward
           break
+        elif (i%FREQUENCY_UPDATE) == 0:
+          agent.update()
+
         state = next_state
-    logs.append(rewards)
+    logs_PER.append(rewards)
+
+  for n in range(2, 16):
+    env = blind_cliff(n) # GAME ENV
+    GAMMA = 1 - 1./n
+
+    # Q-learning without Prioritised Experience Replay
+    agent = Agent(ALPHA, GAMMA, ETA, EPSILON, MEMORY_SIZE, BATCH_SIZE, env.n, env.action_n, is_prioritised=False)
+    rewards = 0
+
+    for i in range(1,NUM_EPISODES):
+      state = env.reset()
+      while True:
+        # if you want to see how the agent moves, open this!
+        # env.render()
+
+        action = agent.policy(state)
+        next_state, reward, done = env.step(action)
+        agent.memory.append((state, action, reward, next_state, done))
+
+        if done:
+          rewards += reward
+          break
+        elif (i%FREQUENCY_UPDATE) == 0:
+          agent.update()
+
+        state = next_state
+    logs_NONPER.append(rewards)
 
 import matplotlib.pyplot as plt
-plt.plot(logs)  
+plt.plot(np.arange(len(logs_PER)), logs_PER, label="PER")
+plt.plot(np.arange(len(logs_NONPER)), logs_NONPER, label="NON PER")
+plt.legend()
 plt.show()
