094f64dfe1f0d86546bdd4692410e306dafc0fdb
Rowing0914 <kosakaboat@gmail.com>
2019-03-20 21:21:43 +0000

    Added

--- DRL/papers/Prioritised_Experience_Replay/blind_cliff.py
+++ DRL/papers/Prioritised_Experience_Replay/blind_cliff.py
@@ -10,7 +10,7 @@ Description:
 """
 
 import numpy as np
-from collections import defaultdict
+from collections import defaultdict, deque
 
 class blind_cliff:
   """
@@ -41,8 +41,8 @@ class blind_cliff:
 
   """
   def __init__(self, n):
-    self.action_n  = 2
-    self.n         = n
+    self.action_n  = 2    # number of action
+    self.n         = n    # state space
     self.env       = ["| |"]*n
     self.agent_pos = 0
     self.env[self.agent_pos] = "|*|"
@@ -61,7 +61,7 @@ class blind_cliff:
     """
     if self.agent_pos == self.n-1:
       # you have reached the terminal state
-      return 0, 1, True
+      return self._decode(), 1, True
     else:
       # if the action was `right`
       if action == 0:
@@ -72,9 +72,9 @@ class blind_cliff:
       # if the action was `wrong`
       elif action == 1:
         # GAME OVer
-        return 0, 0, True
+        return self._decode(), 0, True
       # you have not reached the terminal state
-      return self.agent_pos, 0, False
+      return self._decode(), 0, False
 
   def render(self):
     """
@@ -91,15 +91,32 @@ class blind_cliff:
     self.env[self.agent_pos] = "|*|"
     return 0
 
+  def _decode(self):
+    """
+    this decodes the string format of environemnt to the sequence of number
+    which represents the agent position and other empty space by 1 and 0.
+
+    Sample: assume that state space is 4
+      |*|| || || |  ==> [1,0,0,0]
+      | ||*|| || |  ==> [0,1,0,0]
+      | || ||*|| |  ==> [0,0,1,0]
+    """
+    temp = np.zeros(self.n)
+    temp[self.agent_pos] += 1
+    return temp
+
 class Agent:
   """
   Q-learning agent
   """
-  def __init__(self, Q, alpha, gamma, epsilon):
+  def __init__(self, Q, alpha, gamma, eta, epsilon, memory_size, state_space, action_n):
     self.epsilon = epsilon
     self.Q       = Q
     self.alpha   = alpha
     self.gamma   = gamma
+    self.memory  = deque(maxlen=memory_size) # EXPERIECNE REPLAY MEMORY
+    self.weights = np.random.normal(0, 0.01, [state_space, action_n]) # gaussian distribution: mean 0 and sigma 0.01
+    self.eta     = eta
 
   def __repr__(self):
     """
@@ -115,13 +132,41 @@ class Agent:
     and eventually it will only rely on its learned Q-table
     """
     if np.random.rand() > self.epsilon:
-      action = np.argmax(self.Q[state])
+      action = np.argmax(self.function_approximator(state))
     else:
       action = np.random.choice([0,1], p=[0.5, 0.5])
     self.epsilon *= 0.99
     return action
 
-  def update(self, state, action, reward, next_state):
+  def function_approximator(self, state):
+    """
+    we use a very simple encoding of state as a 1-hot vector (as for tabular)
+
+    Args:
+      state: 1 x n vector(one hot vector) e.g., 
+          Sample: assume that state space is 4
+          |*|| || || |  ==> [1,0,0,0]
+          | ||*|| || |  ==> [0,1,0,0]
+          | || ||*|| |  ==> [0,0,1,0]
+
+    Returns:
+      Q-values: 1 x 2(number of action)
+    """
+    return np.dot(state, self.weights) + 1.0 # 1 is bias
+
+  def update(self, state, action, reward, next_state, done):
+    """
+    Update weights used in function approximation by SGD
+    tricky thing is that the error we want to propagate is not MSE or usual thinsg in ML literature
+    here we use the TD error to learn.
+    """
+    td_delta = reward + self.gamma*np.max(self.function_approximator(next_state)) - self.function_approximator(state)[action]
+    self.weights += self.eta*td_delta*self.function_approximator(state)[action]
+
+  def _memory_maintainance(self):
+    return
+
+  def old_update(self, state, action, reward, next_state, done):
     """
     Update the Q-table
     """
@@ -129,29 +174,35 @@ class Agent:
     self.Q[state][action] += self.alpha*td_delta
 
 if __name__ == '__main__':
-  NUM_STATES = 10
   EPSILON = 0.99
-  GAMMA = 1
-  ALPHA = 0.5
+  ALPHA = 1
+  ETA   = 1./4
   NUM_EPISODES = 100
+  MEMORY_SIZE = 200
+  logs = list()
 
-  env = blind_cliff(NUM_STATES)
-  Q = defaultdict(lambda: np.zeros(env.action_n))
-  agent = Agent(Q, ALPHA, GAMMA, EPSILON)
-  rewards = 0
+  for n in range(2, 16):
+    env = blind_cliff(n) # GAME ENV
+    GAMMA = 1 - 1./n
+    Q = defaultdict(lambda: np.random.normal(0, 0.01, env.action_n)) # Q TABLE
+    agent = Agent(Q, ALPHA, GAMMA, ETA, EPSILON, MEMORY_SIZE, env.n, env.action_n) # Q-LEARNING AGENT
+    rewards = 0
 
-  for i in range(NUM_EPISODES):
-    print("===== GAME: {} =====".format(i))
-    state = env.reset()
-    while True:
-      env.render()
-      action = agent.policy(state)
-      next_state, reward, done = env.step(action)
-      agent.update(state, action, reward, next_state)
-      if done:
-        rewards += reward
-        break
-      state = next_state
+    for i in range(NUM_EPISODES):
+      state = env.reset()
+      while True:
+        # if you want to see how the agent moves, open this!
+        # env.render()
 
-print(agent)
-print(rewards)
+        action = agent.policy(state)
+        next_state, reward, done = env.step(action)
+        agent.update(state, action, reward, next_state, done)
+        if done:
+          rewards += reward
+          break
+        state = next_state
+    logs.append(rewards)
+
+import matplotlib.pyplot as plt
+plt.plot(logs)  
+plt.show()
